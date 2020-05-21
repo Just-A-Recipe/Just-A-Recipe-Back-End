4abea16954e13a18082daa5b6eb7a515e40226f3
james eserjose <james.re@yahoo.com>
2020-05-20 17:34:12 -0700

    card3 routes/test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,8 @@ const { MongoMemoryServer } = require('mongodb-memory-server');
 const mongod = new MongoMemoryServer();
 const mongoose = require('mongoose');
 const connect = require('../lib/utils/connect');
-const Recipe = require('../lib/models/Recipe');
+// const Recipe = require('../lib/models/Recipe');
+require('dotenv').config();
 
 const request = require('supertest');
 const app = require('../lib/app');
@@ -16,42 +17,38 @@ describe('be-cookbook routes', () => {
     return mongoose.connection.dropDatabase();
   });
 
+
   afterAll(() => {
     return mongoose.connection.close();
   });
-  
 
-// const recipe = {
-//   id: 1234,
-//   title: 'BobsBurgers',
-//   image: 'www.image.com',
-//   glutenFree: false,
-//   dairyFree: false,
-//   vegan: false,
-//   summary: 'bobs fantasic super duper delicious mega ultimate burger'
 
-//   it('creates a recipe', () => {
-//     return request(app)
-//       .post('/api/v1/recipes')
-//       .send({
-//         id: 1234,
-//         title: 'BobsBurgers',
-//         image: 'www.image.com',
-//         glutenFree: false,
-//         dairyFree: false,
-//         vegan: false,
-//         summary: 'bobs fantasic super duper delicious mega ultimate burger'
-//       })
-//       .then(res => {
-//         expect(res.body).toEqual({
-//           _id: expect.any(String),
-//           title: 'BobsBurgers',
-//           image: 'www.image.com',
-//           glutenFree: false,
-//           dairyFree: false,
-//           vegan: false,
-//           summary: 'bobs fantasic super duper delicious mega ultimate burger'
-//         });
-//       });
-//   });
-// });
+  const recipe = {
+    // id: 1234,
+    title: 'BobsBurgers',
+    image: 'www.image.com',
+    summary: 'bobs fantasic super duper delicious mega ultimate burger' 
+  };
+  it('creates a recipe', () => {
+    return request(app)
+      .post('/api/v1/recipes')
+      .send({
+        id: 123,
+        title: 'BobsBurgers',
+        image: 'www.image.com',
+        summary: 'bobs fantasic super duper delicious mega ultimate burger'
+        
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          id: 123,
+          title: 'BobsBurgers',
+          image: 'www.image.com',
+          summary: 'bobs fantasic super duper delicious mega ultimate burger',
+          __v:0
+        });
+      });
+  });
+  // return recipe;
+});
